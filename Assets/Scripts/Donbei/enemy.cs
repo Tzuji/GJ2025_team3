5e13c40de67ff92a299e12fd67dd12a54042c9ec
2321025-PC\2321025 <donbei796@gmail.com>
2025-06-09 00:26:26 +0900

    敵づくり頑張ってみたり(未完成)

---
+++ Assets/Scripts/Donbei/enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    private GameObject player;
+    private float time;
+    void Start()
+    {
+        player = GameObject.Find("player");
+        time = Time.deltaTime;
+    }
+
+    void Update()
+    {
+        Vector3 vector3 = player.transform.position - this.transform.position;
+        Quaternion quaternion = Quaternion.LookRotation(vector3);
+        this.transform.rotation = quaternion;
+        if (time > 2f)//２秒に一回発射
+        {
+            Debug.Log("on");
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            time = Time.deltaTime;
+        }
+        time += Time.deltaTime;
+
+    }
+}
--- Assets/Scripts/Donbei/enemy_bullet.cs
+++ Assets/Scripts/Donbei/enemy_bullet.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enemy_bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 2f;
+    private float time = 0f;
     void Update()
     {
-        
+        time %= 360;
+        time *= 6;
+        speed = (float)(Math.Sin(time) + 1) * -20;
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        time += 0.1f;
     }
 }
