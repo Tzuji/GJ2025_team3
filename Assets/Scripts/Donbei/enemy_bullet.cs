8d722c719381a7744baffd7e5a604a4df7f3feb1
2321025-PC\2321025 <donbei796@gmail.com>
2025-06-09 16:29:37 +0900

    玉の速度調整とか。”可”って感じになった

--- Assets/Scripts/Donbei/enemy_bullet.cs
+++ Assets/Scripts/Donbei/enemy_bullet.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class enemy_bullet : MonoBehaviour
 {
     public float speed = 2f;
+
+    public float freq = 1f;
+
     private float time = 0f;
+
     void Update()
     {
-        time %= 360;
-        time *= 6;
-        speed = (float)(Math.Sin(time) + 1) * -20;
+        time = (float)(((180 / freq) * time / Math.PI) % (180 * 360 / Math.PI));
+        speed = -10 + (float)(Math.Sin(time)) * 9;
         transform.Translate(Vector3.up * speed * Time.deltaTime);
-        time += 0.1f;
+        time += 0.5f;
     }
 }
