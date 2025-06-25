2dd191f72b3e69522f1ab6b7db652d747b41ce5c
Tsuji <ituzi27@hotmail.com>
2025-06-25 11:53:41 +0900

    ニコモジフォント追加　リザルトに敵の数と最終的なスコアを表示する

--- Assets/Scripts/tsuzi/ResultText.cs
+++ Assets/Scripts/tsuzi/ResultText.cs
@@ -4,10 +4,23 @@ using TMPro;  // TextMeshPro を使う場合
 public class ResultManager : MonoBehaviour
 {
     public TextMeshProUGUI resultText;
+    public TextMeshProUGUI sumScore;
+    public TextMeshProUGUI ClearBonus;
 
     void Start()
     {
-        int defeated = GameManager.Instance.enemiesDefeated;
-        resultText.text = "倒した敵の数: " + defeated;
+        int s = 0;
+
+        resultText.text = "倒した敵の数: " + GameManager.Instance.enemiesDefeated;
+
+        if (GameManager.Instance.PlayerAlive)
+        {
+            s = 100000000;
+            ClearBonus.text = "クリアボーナス +100000000";
+        }
+
+        s += GameManager.Instance.score;
+        sumScore.text = "スコア: " + s;
+
     }
 }
--- Assets/Scripts/tsuzi/StageManager_tsuzi.cs
+++ Assets/Scripts/tsuzi/StageManager_tsuzi.cs
@@ -5,28 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class StageManager_tsuzi : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    float timer = 0f;
+    public GameObject Player;
 
     void Update()
     {
-        timer += Time.deltaTime;
-
-        // 5秒後に敵1を出す
-        if (timer > 2f && timer < 2.1f)
-        {
-            Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        }
-        
-        if (timer > 4f && timer < 4.1f)
-        {
-            Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        }
-
-        if (timer > 10f && timer < 10.1f)
+        if (Player == null)
         {
             SceneManager.LoadScene("Result");
 
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Result");
         }
     }
 }
