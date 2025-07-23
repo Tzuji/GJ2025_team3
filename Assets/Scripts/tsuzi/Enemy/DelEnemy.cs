55149de8486962489cf406ec200c0333efd31471
Tsuji <ituzi27@hotmail.com>
2025-07-23 23:17:52 +0900

    とりさんが作った敵がホーミング弾で倒せない不具合を修正

--- Assets/Scripts/tsuzi/Enemy/DelEnemy.cs
+++ Assets/Scripts/tsuzi/Enemy/DelEnemy.cs
@@ -5,8 +5,10 @@ public class DelEnemy : MonoBehaviour
     public int point = 1;
     void OnTriggerEnter2D(Collider2D Collider)
     {
+        Debug.Log("hitEnemy");
         if (Collider.gameObject.tag == "PlayerBullet")
         {
+            Debug.Log("DestroyEnemy");
             GameManager.Instance.AddEnemyDefeated();
             GameManager.Instance.AddScore(point);
             Destroy(this.gameObject);
